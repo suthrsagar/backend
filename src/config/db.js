04ca8749f7ed251c2ae4997e08565cab880eed9c
Sagar <sutharsagar710@gmail.com>
2026-07-08 00:41:39 +0530

    fix: change prisma client import to default import for ESM support

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import { DATABASE_URL } from './config.js';
